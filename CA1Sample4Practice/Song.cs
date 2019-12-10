9fcd620b94152f4e3dd3e75b8977580e2c96d8eb
Hannah147 <47416196+Hannah147@users.noreply.github.com>
2019-12-10 15:38:25 +0000

    Add properties and constructors

--- CA1Sample4Practice/Song.cs
+++ CA1Sample4Practice/Song.cs
@@ -10,6 +10,22 @@ namespace CA1Sample4Practice
     // Question 2
     class Song
     {
+        // Question 4
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public double Duration { get; set; }
+        public Genre Genre { get; set; }
 
+        // Question 5
+        public Song(string title, string artist, double duration, Genre genre)
+        {
+            Title = title;
+            Artist = artist;
+            Duration = duration;
+            Genre = genre;
+        }
+
+        public Song(string title) : this(title, "Unknown", 0, Genre.Other) { }
+        public Song() : this("Unknown", "Unknown", 0, Genre.Other) { }
     }
 }
