c829221eec99953282eb969e25ef094b74ceff09
Hannah147 <47416196+Hannah147@users.noreply.github.com>
2019-12-10 15:24:55 +0000

    Added class and enum

---
+++ CA1Sample4Practice/Song.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA1Sample4Practice
+{
+    public enum Genre { Rock, Pop, Dance, Other}
+    // Question 2
+    class Song
+    {
+
+    }
+}
